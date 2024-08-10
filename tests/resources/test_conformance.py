bb831d7b1c1f229c5dd4599ac107975bb65aad79
seskildsen <sines@sdfi.dk>
2024-08-10 12:46:19 +0200

    tests: check if landingpage URL's return with the correct prefix

--- tests/resources/test_conformance.py
+++ tests/resources/test_conformance.py
@@ -5,7 +5,15 @@ import pytest
 
 @pytest.fixture
 def response(app_client):
-    return app_client.get("/")
+    # return app_client.get("/")
+    return app_client.get(
+            "/",         
+            headers={
+                "X-Forwarded-Proto": "https",
+                "X-Forwarded-Host": "api.dataforsyningen.dk",
+                "X-Forwarded-Prefix": "/rest/skraafoto_api/v2/"
+            },
+        )
 
 
 @pytest.fixture
@@ -30,21 +38,35 @@ def test_landing_page_health(response):
 #  - Rel type of link to test
 #  - Expected MIME/Media Type
 #  - Expected relative path
+# link_tests = [
+#     ("self", "application/json", "/"),
+#     ("root", "application/json", "/"),
+#     ("data", "application/json", "/collections"),
+#     ("conformance", "application/json", "/conformance"),
+#     ("search", "application/geo+json", "/search"),
+#     ("http://www.opengis.net/def/rel/ogc/1.0/queryables", "application/schema+json", "/queryables"),
+#     ("service-doc", "text/html", "/api.html"),
+#     ("service-desc", "application/vnd.oai.openapi+json;version=3.0", "/api"),
+# ]
+
 link_tests = [
-    ("self", "application/json", "/"),
-    ("root", "application/json", "/"),
-    ("data", "application/json", "/collections"),
-    ("conformance", "application/json", "/conformance"),
-    ("search", "application/geo+json", "/search"),
-    ("http://www.opengis.net/def/rel/ogc/1.0/queryables", "application/schema+json", "/queryables"),
-    ("service-doc", "text/html", "/api.html"),
-    ("service-desc", "application/vnd.oai.openapi+json;version=3.0", "/api"),
+    ("self", "application/json", "/rest/skraafoto_api/v2/", "/"),
+    ("root", "application/json", "/rest/skraafoto_api/v2/", "/"),
+    ("data", "application/json", "/rest/skraafoto_api/v2/collections", "/collections"),
+    ("conformance", "application/json", "/rest/skraafoto_api/v2/conformance", "/conformance"),
+    ("search", "application/geo+json", "/rest/skraafoto_api/v2/search", "/search"),
+    ("http://www.opengis.net/def/rel/ogc/1.0/queryables", "application/schema+json", "/rest/skraafoto_api/v2/queryables", "/queryables"),
+    ("service-doc", "text/html", "/rest/skraafoto_api/v2/api.html", "/api.html"),
+    ("service-desc", "application/vnd.oai.openapi+json;version=3.0", "/rest/skraafoto_api/v2/api", "/api"),
 ]
 
-
-@pytest.mark.parametrize("rel_type,expected_media_type,expected_path", link_tests)
+# @pytest.mark.parametrize("rel_type,expected_media_type,expected_path", link_tests)
+# def test_landing_page_links(
+#     response_json, app_client, rel_type, expected_media_type, expected_path
+# ):
+@pytest.mark.parametrize("rel_type,expected_media_type,expected_path,request_path", link_tests)
 def test_landing_page_links(
-    response_json, app_client, rel_type, expected_media_type, expected_path
+    response_json, app_client, rel_type, expected_media_type, expected_path, request_path
 ):
     link = get_link(response_json, rel_type)
 
@@ -54,7 +76,8 @@ def test_landing_page_links(
     link_path = urllib.parse.urlsplit(link.get("href")).path
     assert link_path == expected_path
 
-    resp = app_client.get(link_path)
+    # resp = app_client.get(link_path)
+    resp = app_client.get(request_path)
     assert resp.status_code == 200
 
 
